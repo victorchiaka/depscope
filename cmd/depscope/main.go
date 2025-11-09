7cc186083d32049b0dde6ddd261a39c806b8bdc5
victorchiaka <chiakavictor205@gmail.com>
2025-11-09 13:57:48 +0100

    fix

--- cmd/depscope/main.go
+++ cmd/depscope/main.go
@@ -34,9 +34,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
 
+	"github.com/victorchiaka/depscope"
 	"github.com/victorchiaka/depscope/internal/server"
 )
 
@@ -44,18 +46,17 @@ func main() {
 	port := flag.String("port", "4000", "Port to run server on")
 	flag.Parse()
 
-	// Serve static files
-	fs := http.FileServer(http.Dir("web"))
-	http.Handle("/script.js", fs)
-	http.Handle("/style.css", fs)
+	webFiles, err := fs.Sub(depscope.WebFS, "web")
+	if err != nil {
+		log.Fatal("Failed to load web files:", err)
+	}
 
-	// API endpoint
+	http.Handle("/script.js", http.FileServer(http.FS(webFiles)))
+	http.Handle("/style.css", http.FileServer(http.FS(webFiles)))
 	http.HandleFunc("/api/graph", server.Root)
-
-	// Web page
-	http.HandleFunc("/", server.Web)
+	http.Handle("/", http.FileServer(http.FS(webFiles)))
 
 	addr := ":" + *port
-	fmt.Printf("🔍 Depscope running at http://localhost%s\n", addr)
+	fmt.Printf("🔍 DepScope running at http://localhost%s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
